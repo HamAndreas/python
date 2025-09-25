7d57e242045249851ebab541b4e0485f1688b62e
Andreas Hamann <ahamann87@gmail.com>
2025-09-25 12:59:57 +0200

    add text_gen.py

---
+++ text_generator.py
@@ -0,0 +1,9 @@
+# start
+name = "Andreas"
+country = "Germany"
+age = "38"
+result = None
+
+result = f"Hello, my name is "+name+ ", I'm " + age + " years old and I come from " + country+"."
+print(result)
+# end
