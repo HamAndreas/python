0d74765174d29cb898275ebebba1ffdad04ed7da
Andreas Hamann <ahamann87@gmail.com>
2025-10-02 11:00:58 +0200

    modified ageMachine.py

--- ageMachine.py
+++ ageMachine.py
@@ -1,7 +1,22 @@
-def ageMachine():
-    print("I am ageMachine function")
+"""
+this function takes integer age as argument and print if the person is old or young
+"""
 
-def jojo():
-    print("I am jojo function")
-ageMachine()
-jojo()
+def ageMachine(age: int):
+    if age >= 50:
+        print("Du bist alt!")
+    else:
+        print("Du bist jung!")
+
+"""
+this function accepts two integer as arguments and adds them
+returns result of summation
+"""
+# mercedes_auto     snake_case can be used for variables
+def addTwoNumers(first_num: int , second_num: int)-> int:
+    return first_num + second_num
+
+ageMachine(49.999999)
+
+result = addTwoNumers(5, 10)
+print(f"It is: {result}")
