60dff8f1407a051ef8c19bcbff871492e4cc99e3
Andreas Hamann <ahamann87@gmail.com>
2025-10-24 12:11:05 +0200

    add file multiplikation.py

---
+++ Übungen Cakir/Aufgabe VIII while-Schleife/3 - multiplikation.py
@@ -0,0 +1,11 @@
+x=int(2)
+y=int(1)
+ergebnis=int()
+
+print(f"----y----x")
+while ergebnis<50:
+    ergebnis=x*y
+    print(f"{x} x {y} = {ergebnis}")
+    y+=1    
+
+print("Programm wird beendet, da der Maximalwert von 50 erreicht ist.")
